c9e71c5eed44d17b810e43f2ef0436d9850a941f
Fett-Boba <thugfencer@gmail.com>
2023-08-09 21:34:37 -0700

    Cleanup script

--- utils/src/main/resources/scripts/createTestsJson.sh
+++ utils/src/main/resources/scripts/createTestsJson.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Create the tests.json file.
+# $1 = the directory
+
 # Make sure directory was passed via command line
 if [ $# -eq 0 ]; then
     echo "Usage: $0 <directory>"
@@ -10,18 +13,24 @@ fi
 DIR="$1"
 cd "$DIR"
 
-# Store names of the directories, read into the array
+# Store names of the directories in array
 directories=()
 while read -r line; do
     directories+=("$line")
 done < <(ls -d */ | sed 's/\/$//')
 
-# Write the opening bracket to the file
+# Write the opening bracket to the output file
 echo "[" > tests.json
 
-# Write names of the directories stored in the array to a file
+# Write out the json (no trailing comma for last directory)
+counter=0
 for dir in "${directories[@]}"; do
-    echo -e " {\n  \"name\": \"$dir\",\n  \"description\": \"Acct type codes starting with $dir\"\n }," >> tests.json
+  if [[ ! $counter -eq $((${#directories[@]}-1)) ]]; then
+      echo -e " {\n  \"name\": \"$dir\",\n  \"description\": \"Acct type codes starting with $dir\"\n }," >> tests.json
+  else
+      echo -e " {\n  \"name\": \"$dir\",\n  \"description\": \"Acct type codes starting with $dir\"\n }" >> tests.json
+  fi
+  ((counter++))
 done
 
 # Write the closing bracket to the file
